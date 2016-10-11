573d3bbfc478ce3ef5ee759f81c752277f7203fb
Graham Pegg (MacBook) <graham@citrusmedia.com>
2016-10-11 13:23:34 -0400

    DRY gallery viewer

--- vue/main.js
+++ vue/main.js
@@ -1,15 +1,26 @@
 import Vue from 'vue';
+import VueResource from 'vue-resource';
+
 import spinIn from './spin-in.vue';
 import pageBreak from './page-break.vue';
+import largeImg from './large-img.vue';
+
+Vue.use(VueResource);
 
 var vm = new Vue({
 	el: '.wrapper',
 	data: {
 		scroll: 0
 	},
+	methods: {
+		queryURL (name) {
+			return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
+		}
+	},
 	components: {
 		spinIn,
-		pageBreak
+		pageBreak,
+		largeImg
 	}
 })
 
